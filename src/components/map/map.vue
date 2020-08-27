619fbb27cdef23a2605f1afda090799686250329
gqg <184416297@qq.com>
2020-08-27 18:09:13 +0800

    地图引入

---
+++ src/components/map/map.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="map" class="container"></div>
+</template>
+<script>
+import * as maptalks from 'maptalks';
+export default {
+  data(){
+    return {
+
+    }
+  },
+  mounted(){
+    this.$nextTick(()=>{
+      const map = new maptalks.Map('map',{
+        center: [-0.113049, 51.498568],
+        zoom: 14,
+        baseLayer: new maptalks.TileLayer('base',{
+          urlTemplate: 'http://online{s}.map.bdimg.com/onlinelabel/?qt=tile&x={x}&y={y}&z={z}&s=1&styles=pl&scaler=1&p=1&s=1',
+          subdomains: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+        })
+      })
+      console.log('map:',map)
+    })
+  }
+}
+</script>
+
+<style lang="less">
+.container{ width:100%;height:100% }
+</style>
---
+++ src/page/index/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="index">
+    <div class="title">地图练习</div>
+    <gqg-map></gqg-map>
+  </div>
+</template>
+<script>
+import gqgMap from "@/components/map/map.vue";
+export default {
+  components:{
+    gqgMap
+  },
+  data() {
+    return {};
+  },
+  mounted() {},
+  methods: {}
+};
+</script>
+<style scoped lang="less"></style>
